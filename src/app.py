acf88ae3b66314d2cb7c22a86367018e2592dcf5
Andrea <andrea.baron32@gmail.com>
2022-08-05 23:24:32 +0000

    avance

--- src/app.py
+++ src/app.py
@@ -1,3 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # hace referencia al nombre del archivo: app
+
+@app.route('/')
+def hello_flask():
+    return 'Hello Flask'
+
+# le decimos a Flask que corra
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000) #lo hacemos en modalidad debug
