2c8c70a9933e0689fa20b74e9c0210c9f10982d7
Andrea <andrea.baron32@gmail.com>
2022-08-06 00:10:43 +0000

    avance

--- src/app.py
+++ src/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__) # hace referencia al nombre del archivo: app
 
 @app.route('/')
 def hello_flask():
     return 'Hello Flask'
+
+@app.route('/inicio') #inicio nos lleva a la pag web
+def show_home():
+    return render_template('index.html') #le ponemos el nombre de nuestra pagina web
+
+@app.route('/url_variables/<string:name>/<int:age>') #le pasamos parametros a la url
+def url_variables(name, age):
+    if age<18:
+        return jsonify(message = 'Lo siento ' + name + ' no estas autorizado'), 401 # 401 es el error
+    else:
+        return jsonify(message = 'Bienvenida ' + name), 200 # por default ya viene 200, no es necesario ponerlo
+
+
 
 # le decimos a Flask que corra
 if __name__ == '__main__':
